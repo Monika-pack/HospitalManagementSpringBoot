c4af6d15fe46a370196d4f7fa255b8db514053fc
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-04-30 08:26:34 -0400

    Add files via upload

---
+++ SpringDepartmentCrudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDepartmentCrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDepartmentCrudeApplication.class, args);
+	}
+
+}
